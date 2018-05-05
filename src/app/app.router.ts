1554e57c5df9c36af78189e9ec40edabe1e9234f
antoniotex <acm.skt@gmail.com>
2018-05-04 22:47:28 -0300

    Add rotas para catalogo, detalhes e cadastro produto

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { DetalhesComponent } from './detalhes/detalhes.component';
+import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
 
 const ROTAS: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'catalogo', component: CatalogoComponent },
+    { path: 'detalhes', component: DetalhesComponent },
+    { path: 'cadastro-produto', component: CadastroProdutoComponent }
 ];
